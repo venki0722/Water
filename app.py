8b1cd8ecdbc3aedde6e1aa27dfeed71d0d986dcb
venki0722 <mvenkatachalam633@gmail.com>
2026-04-10 19:47:22 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pickle
+import numpy as np
+
+# Load model
+model = pickle.load(open("model.pkl", "rb"))
+
+st.title("🌾 Crop Prediction App")
+
+# Inputs from user
+temp = st.number_input("Temperature")
+rainfall = st.number_input("Rainfall")
+humidity = st.number_input("Humidity")
+
+# Predict button
+if st.button("Predict"):
+    input_data = np.array([[temp, rainfall, humidity]])
+    prediction = model.predict(input_data)
+
+    st.success(f"Prediction: {prediction[0]}")
